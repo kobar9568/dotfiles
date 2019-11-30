0d1f1b7391c7c06048efcf38d61f7eb69be8385b
Ryota Kobayashi <38117745+kobar9568@users.noreply.github.com>
2019-11-30 17:52:41 +0900

    [new] SSH config

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# dotfiles
 readonly DOTFILES=(.vimrc .gitconfig)
 
+# link
 for file in ${DOTFILES[@]}
 do
   ln -s $HOME/dotfiles/src/$file $HOME/$file
 done
+
+# SSH config
+mkdir $HOME/.ssh/
+ln -s $HOME/dotfiles/src/.ssh/config $HOME/.ssh/config
