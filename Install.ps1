4c606f76d1dac2242d69141324c8f5ab90cc2527
Ryota Kobayashi <38117745+kobar9568@users.noreply.github.com>
2020-01-02 23:58:49 +0900

    [new] Windows用のインストールスクリプトとGUI系config

---
+++ Install.ps1
@@ -0,0 +1,8 @@
+New-Item -Value 'src\.ssh\config' -Path '..\.ssh' -Name 'config' -ItemType SymbolicLink
+New-Item -Value '.\src\.gitconfig' -Path '..\' -Name '.gitconfig' -ItemType SymbolicLink
+New-Item -Value '.\src\.vimrc' -Path '..\' -Name '.vimrc' -ItemType SymbolicLink
+
+New-Item -Value 'src\vscode\settings.json' -Path '..\AppData\Roaming\Code\User' -Name 'settings.json' -ItemType SymbolicLink
+New-Item -Value 'src\vscode\keybindings.json' -Path '..\AppData\Roaming\Code\User' -Name 'keybindings.json' -ItemType SymbolicLink
+
+New-Item -Value 'src\.hyper.js' -Path '..\AppData\Roaming\Hyper' -Name '.hyper.js' -ItemType SymbolicLink
