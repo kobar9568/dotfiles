94cdd392ff6871dbac11a1a4298dc8fb3fdb05cc
Ryota Kobayashi <38117745+kobar9568@users.noreply.github.com>
2020-02-09 09:34:32 +0000

    [change] POSIX mode

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 # dotfiles
-readonly DOTFILES=(.bashrc .vimrc .gitconfig)
+readonly DOTFILES=`find src/home/ -type f | sed 's/^.*home\///'`
 
 # link
-for file in ${DOTFILES[@]}
+for file in $DOTFILES
 do
-  ln -s $HOME/dotfiles/src/$file $HOME/$file
+  ln -s $HOME/dotfiles/src/home/$file $HOME/$file
 done
 
 # SSH config
