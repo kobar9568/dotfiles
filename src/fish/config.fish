6870f94b8de7024756751ca29bd93b309d5584f6
Ryota Kobayashi <38117745+kobar9568@users.noreply.github.com>
2020-08-12 23:54:28 +0900

    feat: config.fish

--- src/fish/config.fish
+++ src/fish/config.fish
@@ -1,5 +1,15 @@
 alias ls="ls --color=auto"
+
+# Alias
 alias ll="ls -alhF"
+alias la="ls -A"
+alias l="ls -CF"
+alias rm="rm -i"
+alias mv="mv -i"
+alias cp="cp -i"
+alias ..="cd .."
+alias date="date --iso-8601=seconds"
+alias less="less -MR"
 
 # グリーティングを非表示
 set fish_greeting
