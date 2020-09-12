a4b38af93a8e7f61726c1b415ce2bf3ed53a92f9
Ryota Kobayashi <38117745+kobar9568@users.noreply.github.com>
2020-09-12 21:28:50 +0900

    feat: coc-settings.json

--- install.sh
+++ install.sh
@@ -41,3 +41,7 @@ ln -s $HOME/dotfiles/src/htop/htoprc $HOME/.config/htop/htoprc
 # starship config
 mkdir -p $HOME/.config/
 ln -s $HOME/dotfiles/src/starship/starship.toml $HOME/.config/starship.toml
+
+# vim config
+mkdir -p $HOME/.vim/
+ln -s $HOME/dotfiles/src/vim/coc-settings.json $HOME/.vim/coc-settings.json
