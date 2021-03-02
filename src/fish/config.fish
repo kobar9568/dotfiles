3119901eb65d92a8f81a70ac4a86a46ec7b2252a
Ryota Kobayashi <38117745+kobar9568@users.noreply.github.com>
2021-03-02 12:55:56 +0900

    chore: refact

--- src/fish/config.fish
+++ src/fish/config.fish
@@ -4,15 +4,20 @@ alias ls="ls --color=auto"
 alias ls="exa"
 alias ll="exa -laaghHm --git --time-style long-iso"
 alias la="exa -aa"
+
 alias rm="rm -i"
 alias mv="mv -i"
 alias cp="cp -i"
+
 alias ..="cd .."
+
 alias date="date --iso-8601=seconds"
 alias less="less -MR"
+
 alias stu="git status"
 alias gd="git diff"
 alias gdv="git difftool"
+
 alias hd="hexyl"
 alias tkill="tmux kill-server"
 
