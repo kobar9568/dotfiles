2d493c3d41dd3861c9665eccaf1cdbc94bc6b0a1
Ryota Kobayashi <38117745+kobar9568@users.noreply.github.com>
2023-07-02 04:08:24 +0900

    [fish] fix: fzf preview.

--- src/fish/do_ctrl_g.fish
+++ src/fish/do_ctrl_g.fish
@@ -3,7 +3,11 @@ function do_ctrl_g
 
   set before_dir (pwd)
 
-  cd (ghq root)"/"(ghq list | fzf --preview "batcat --color=always --style=header,grid --line-range :80 \$(ghq root)/{}/README.*")
+  if type "batcat" > /dev/null 2>&1
+    cd (ghq root)"/"(ghq list | fzf --preview "batcat --color=always --style=header,grid --line-range :80 \$(ghq root)/{}/README.*")
+  else if type "bat" > /dev/null 2>&1
+    cd (ghq root)"/"(ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :80 \$(ghq root)/{}/README.*")
+  end
 
   if not test $before_dir = (pwd)
     ll
