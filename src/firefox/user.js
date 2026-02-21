a45fa9cee53460798d19a0c2e42a6d60c7e7e6c8
Ryota Kobayashi <38117745+kobar9568@users.noreply.github.com>
2026-02-22 03:39:02 +0900

    [Firefox] feat: Add user_pref.

--- src/firefox/user.js
+++ src/firefox/user.js
@@ -1,1 +1,4 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // Default: false
+user_pref("browser.tabs.loadBookmarksInBackground", true); // Default: false
+user_pref("browser.bookmarks.openInTabClosesMenu", false); // Default: true
+user_pref("taskbar.grouping.useprofile", true); // Default: N/A
